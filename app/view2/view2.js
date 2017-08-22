21f20467fbed7d3afc32209ab5eb76d0a783800c
gmobi-zl <liang.zhang@generalmobi.net>
2017-08-23 00:25:31 +0800

    底部栏位，过滤选择器

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -12,6 +12,14 @@ angular.module('adRep.view2', ['ngRoute'])
 .controller('AdsListController', ['$scope', '$location', function($scope, $location) {
 
   $scope.adsRecords = adsReporterData;
+  $scope.filterTypeLabel = "日期";
+  $scope.filterTypeList = [{"label":"2017-08-23"},
+      {"label":"2017-08-22"},
+      {"label":"2017-08-21"},
+      {"label":"2017-08-20"},
+      {"label":"2017-08-19"},
+      {"label":"2017-08-18"},
+      {"label":"2017-08-17"}];
   
   $scope.adsListClick = function (index) {
       currentAd = adsReporterData[index];
